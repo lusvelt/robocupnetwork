b7548c20b3f614e9bd5f2401c9fc01b094ac9e4c
Niccolò Bellucci <niccolo.bellucci.dev@gmail.com>
2018-09-09 18:17:35 +0200

    Database connection improvement

--- server/config/database.js
+++ server/config/database.js
@@ -4,7 +4,15 @@ const User = require('../models/User');
 
 const database = {
     initialize: async () => {
-        await sequelize.sync({ force: process.env.NODE_ENV !== 'production' });
+      await sequelize.authenticate().then( () => {
+        console.log('Connection has been established successfully.');
+        sequelize.sync({
+          force: process.env.NODE_ENV !== 'production'
+        });
+      })
+        .catch(err => {
+          console.error('Unable to connect to the database:', err);
+      });
     }
 };
 
